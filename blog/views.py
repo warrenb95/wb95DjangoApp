ba8937867c1a9e6e2d5a7248f3abbbbf1f78a66a
warren <warrenb95@live.co.uk>
2019-02-23 10:25:26 +0000

    Added meta descriptions and google meta tag

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,8 @@ def photography(request):
 
 	params = {
 		'title' : 'Photography Blog',
-		'blog_posts' : BlogPost.objects.all().order_by('-date_posted')
+		'blog_posts' : BlogPost.objects.all().order_by('-date_posted'),
+		"meta_desc" : "wb95 blog. Here you will find all the blogs written by wb95."
 	}
 
 	return render(request, 'blog/photography.html', params)
@@ -18,7 +19,8 @@ def photographyPost(request, post_slug):
 
 	params = {
 		'post': blogPost,
-		'sections': blogPost.section.all()
+		'sections': blogPost.section.all(),
+		"meta_desc" : "wb95 blog post."
 	}
 
 	return render(request, 'blog/photographyDetail.html', params)
--- photography/views.py
+++ photography/views.py
@@ -6,7 +6,8 @@ from .models import GalleryGroup
 def home(request):
 
 	params = {
-		"title" : "Home"
+		"title" : "Home",
+		"meta_desc" : "wb95 is the official creative website of photographer, Warren Brown. This is the home page of wb95."
 	}
 
 	return render(request, 'photography/home.html', params)
@@ -14,7 +15,8 @@ def home(request):
 
 def contact(request):
 	params = {
-		"title" : "Contact"
+		"title" : "Contact",
+		"meta_desc" : "This is the contact page of wb95, please feel free to use this or send an email to wb95.images@gmail.com."
 	}
 	return render(request, 'photography/contact.html', params)
 
@@ -24,5 +26,6 @@ def gallery(request):
 	params = {
 		'title': 'Gallery',
 		'gallery_groups': GalleryGroup.objects.all(),
+		"meta_desc" : "This is the gallery of wb95. Displaying a selction of images taken by wb95."
 	}
 	return render(request, 'photography/gallery.html', params)
